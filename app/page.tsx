edd5c541b1721b2e3bf228b532bf45fe6838fad3
Your Name <horengkr@gmail.com>
2025-12-30 07:30:51 +0900

    nationality blanked

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   const [formData, setFormData] = useState({
     fullName: '', 
     gender: 'Male', 
-    nationality: 'USA', 
+    nationality: '', 
     style: 'Trendy', 
     userToken: ''
   });
